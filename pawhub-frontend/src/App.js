c08991cb4175b81172ce27717d7e15136b06cb12
t-iffany <tiffanyxleong@gmail.com>
2023-02-07 18:10:34 -0800

    Add login route, create login_controller, add basic auth to Login.jsx

--- pawhub-frontend/src/App.js
+++ pawhub-frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import DiscussionList from './components/DiscussionList';
 import NavigationBar from './components/NavigationBar';
+import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 function App() {
+
   return (
     <Router>
       <div className="App">
@@ -13,7 +14,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/discussions" element={<DiscussionList />} />
-
+            <Route path="/login" element={<Login />} /> 
           </Routes>
         </div>
       </div>
